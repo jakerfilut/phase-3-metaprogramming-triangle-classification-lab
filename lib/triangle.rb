dc3bc4ce1e9f731d35f847e3945ed4473b7fb60c
Jake <jakerfilut@gmail.com>
2021-12-15 22:22:38 -0700

    commit tree c82904896c9135fb9217a4a5a8b6998394fa4151

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def valid?
+    ([a, b, c].all?(&:positive?)) && (a + b > c && a + c > b && b + c > a)
+  end
+
+  def validate_triangle
+    raise TriangleError unless valid?
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
